427262a3c166a89fb8248e1d1cbb9c5dcdaf2c12
Sinisterzx <luk3fcunningham@gmail.com>
2024-06-11 11:04:31 +0100

    Added a cube that can rotate using javafx

--- src/main/java/code/Gui.java
+++ src/main/java/code/Gui.java
@@ -1,92 +1,81 @@
 package code;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
-import javafx.scene.Node;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
-import javafx.scene.effect.BlendMode;
-import javafx.scene.effect.BoxBlur;
+import javafx.scene.paint.*;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Stop;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.StrokeType;
+import javafx.scene.shape.*;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
-import javafx.util.Duration;
-
-import static java.lang.Math.random;
+import java.util.concurrent.atomic.AtomicReference;
 
 
 public class Gui extends Application {
 
+    @Override
+    public void start(Stage primaryStage) {
+
+        int x_position = 400;
+        int y_position = 300;
+        PhongMaterial material = new PhongMaterial();
+        material.setDiffuseColor(Color.GREEN);
 
 
-        @Override
-        public void start(Stage primaryStage) {
-            Group root = new Group();
-            Scene scene = new Scene(root, 800, 600, Color.BLACK);
-            primaryStage.setScene(scene);
-
-            Group circles = new Group();
-            for (int i = 0; i < 30; i++) {
-                Circle circle = new Circle(150, Color.web("white", 0.05));
-                circle.setStrokeType(StrokeType.OUTSIDE);
-                circle.setStroke(Color.web("white", 0.16));
-                circle.setStrokeWidth(4);
-                circles.getChildren().add(circle);
-            }
-
-            Rectangle colors = new Rectangle(scene.getWidth(), scene.getHeight(),
-                    new LinearGradient(0f, 1f, 1f, 0f, true, CycleMethod.NO_CYCLE, new
-                            Stop[]{
-                            new Stop(0, Color.web("#f8bd55")),
-                            new Stop(0.14, Color.web("#c0fe56")),
-                            new Stop(0.28, Color.web("#5dfbc1")),
-                            new Stop(0.43, Color.web("#64c2f8")),
-                            new Stop(0.57, Color.web("#be4af7")),
-                            new Stop(0.71, Color.web("#ed5fc2")),
-                            new Stop(0.85, Color.web("#ef504c")),
-                            new Stop(1, Color.web("#f2660f")),}));
-            colors.widthProperty().bind(scene.widthProperty());
-            colors.heightProperty().bind(scene.heightProperty());
-            Group blendModeGroup =
-                    new Group(new Group(new Rectangle(scene.getWidth(), scene.getHeight(),
-                            Color.BLACK), circles), colors);
-            colors.setBlendMode(BlendMode.OVERLAY);
-            root.getChildren().add(blendModeGroup);
-
-            circles.setEffect(new BoxBlur(10, 10, 3));
-
-            Timeline timeline = new Timeline();
-            for (Node circle: circles.getChildren()) {
-                timeline.getKeyFrames().addAll(
-                        new KeyFrame(Duration.ONE, // set start position at 0
-                                new KeyValue(circle.translateXProperty(), random() * 800),
-                                new KeyValue(circle.translateYProperty(), random() * 600)
-                        ),
-                        new KeyFrame(new Duration(40000), // set end position at 40s
-                                new KeyValue(circle.translateXProperty(), random() * 800),
-                                new KeyValue(circle.translateYProperty(), random() * 600)
-                        )
-                );
-            }
-
-            timeline.play();
+        final Box box = new Box();
+        box.setHeight(200);
+        box.setWidth(200);
+        box.setDepth(200);
+        box.setMaterial(material);
+        box.setLayoutX(x_position);
+        box.setLayoutY(y_position);
 
 
-            primaryStage.setTitle("Rubiks Cube");
-            primaryStage.show();
-        }
+        Group root = new Group(box);
+        Scene scene = new Scene(root, 800, 600, Color.BLACK);
 
-        public void begin() {
-            launch();
-        }
+
+        AtomicReference<Double> initial_X = new AtomicReference<>(0.0);
+        AtomicReference<Double> initial_Y = new AtomicReference<>(0.0);
+        AtomicReference<Double> initial_Angle_X = new AtomicReference<>(0.0);
+        AtomicReference<Double> initial_Angle_Y = new AtomicReference<>(0.0);
+
+
+        Rotate rotateX = new Rotate(0, Rotate.X_AXIS);
+        Rotate rotateY = new Rotate(0, Rotate.Y_AXIS);
+        rotateX.setPivotX(x_position);
+        rotateX.setPivotY(y_position);
+        rotateY.setPivotX(x_position);
+        rotateY.setPivotY(y_position);
+        root.getTransforms().addAll(rotateX, rotateY);
+
+
+        root.setOnMousePressed(event -> {
+            initial_X.set(event.getSceneX());
+            initial_Y.set(event.getSceneY());
+            initial_Angle_X.set(rotateX.getAngle());
+            initial_Angle_Y.set(rotateY.getAngle());
+        });
+
+
+        scene.setOnMouseDragged(event -> {
+            double x = event.getSceneX() - initial_X.get();
+            double y = event.getSceneY() - initial_Y.get();
+
+            rotateX.setAngle(initial_Angle_X.get() + y);
+            rotateY.setAngle(initial_Angle_Y.get() - x);
+        });
+
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Rubiks Cube");
+        primaryStage.show();
+    }
+
+    public void begin() {
+        launch();
+    }
 
 }
